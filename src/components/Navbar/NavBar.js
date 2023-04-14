723774ade139c1ad3d86da24cce513341be651b4
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-04-15 02:41:07 +0530

    new

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,63 +1,69 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
-import logo from "../../assets/website-logo.svg"
-import {Primeform} from "../../components/consultation_form/Primeform.js"
+import logo from "../../assets/website-logo.svg";
+import { Primeform } from "../../components/consultation_form/Primeform.js";
 
 function NavBar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
-  const [displayform,setDisplayForm] = useState(false);
+  const [displayform, setDisplayForm] = useState(false);
+
   const formshow = () => setDisplayForm(true);
   return (
     <>
-    <div className={displayform?"form-page-hide":"form-page-show"}><Primeform/></div>
-      <nav className="navbar">
-        <div className="nav-container">
-
-          <NavLink exact to="/" className="nav-logo">
-            <img src={logo} alt="" /> 
-            <span className="nav-logo-name">The<strong>WEB3</strong>Guys</span>
+      <div className={displayform ? "form-page-hide" : "form-page-show"}>
+        <Primeform setDisplayForm={setDisplayForm} />
+      </div>
+      <nav className='navbar'>
+        <div className='nav-container'>
+          <NavLink exact to='/' className='nav-logo'>
+            <img src={logo} alt='' />
+            <span className='nav-logo-name'>
+              The<strong>WEB3</strong>Guys
+            </span>
           </NavLink>
 
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
+            <li className='nav-item'>
               <NavLink
                 exact
-                to="/"
-                activeClassName="active"
-                className="nav-links"
+                to='/'
+                activeClassName='active'
+                className='nav-links'
                 onClick={handleClick}
               >
                 Home
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li className='nav-item'>
               <NavLink
                 exact
-                to="/about"
-                activeClassName="active"
-                className="nav-links"
+                to='/about'
+                activeClassName='active'
+                className='nav-links'
                 onClick={handleClick}
               >
                 About
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li className='nav-item'>
               <NavLink
                 exact
-                to="/services"
-                activeClassName="active"
-                className="nav-links"
+                to='/services'
+                activeClassName='active'
+                className='nav-links'
                 onClick={handleClick}
               >
                 Services
               </NavLink>
             </li>
           </ul>
-            <button className="nav-ctf" onClick={formshow}>Book a free Call</button>
-          <div className="nav-icon" onClick={handleClick}>
+          <button className='nav-ctf' onClick={formshow}>
+            Book a free Call
+          </button>
+          <div className='nav-icon' onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
           </div>
         </div>
--- src/components/consultation_form/Primeform.js
+++ src/components/consultation_form/Primeform.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/website-logo.svg";
 import "./primeform.css";
 import user from "../../assets/user.svg";
@@ -11,144 +11,131 @@ import right_arrow from "../../assets/right_arrow.svg";
 import { useForm, ValidationError } from "@formspree/react";
 
 function ContactForm() {
-	const [state, handleSubmit] = useForm("mnqygdzv");
-	if (state.succeeded) {
-		return <p className="form-success">Thanks for joining!</p>;
-	}
-	return (
-		<form onSubmit={handleSubmit} autocomplete="off">
-			<div className="form-element-outer">
-				<div className="form-element">
-					<img src={user} alt="" />
-					<input
-						id="Name"
-						type="text"
-						name="Name"
-						className="form-field"
-						placeholder="Enter Your Name"
-						required
-					/>
-					<ValidationError
-						prefix="Name"
-						field="Name"
-						errors={state.errors}
-					/>
-				</div>
-				<div className="form-element">
-					<img src={buisness_suit} alt="" />
-					<input
-						id="company-name"
-						type="text"
-						name="company-name"
-						className="form-field"
-						placeholder="Enter Company Name"
-						required
-					/>
-					<ValidationError
-						prefix="company-name"
-						field="company-name"
-						errors={state.errors}
-					/>
-				</div>
-				<div className="form-element">
-					<img src={email} alt="" />
-					<input
-						id="email"
-						type="email"
-						name="email"
-						className="form-field"
-						placeholder="Enter Your Email"
-						required
-					/>
-					<ValidationError
-						prefix="email"
-						field="email"
-						errors={state.errors}
-					/>
-				</div>
-				<div className="form-element">
-					<img src={website_url} alt="" />
-					<input
-						id="website-url"
-						type="url"
-						name="website-url"
-						className="form-field"
-						placeholder="Website URL"
-						required
-					/>
-					<ValidationError
-						prefix="website-url"
-						field="website-url"
-						errors={state.errors}
-					/>
-				</div>
-				<div className="form-element">
-					<img src={phone} alt="" />
-					<input
-						id="phone-number"
-						type="tel"
-						name="phone-number"
-						className="form-field"
-						placeholder="Enter Phone Number"
-						required
-					/>
-					<ValidationError
-						prefix="phone-number"
-						field="phone-number"
-						errors={state.errors}
-					/>
-				</div>
-				<div className="form-element">
-					<img src={phone} alt="" />
-					<input
-						id="budget"
-						type="number"
-						name="budget"
-						className="form-field"
-						placeholder="Enter Monthly Budget"
-						required
-					/>
-					<ValidationError
-						prefix="budget"
-						field="budget"
-						errors={state.errors}
-					/>
-				</div>
-			</div>
-			<button
-				type="submit"
-				className="form-button"
-				disabled={state.submitting}
-			>
-				Save & Continue
-				<img src={right_arrow} alt="" />
-			</button>
-		</form>
-	);
+  const [state, handleSubmit] = useForm("mnqygdzv");
+
+  if (state.succeeded) {
+    return <p className='form-success'>Thanks for joining!</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit} autocomplete='off'>
+      <div className='form-element-outer'>
+        <div className='form-element'>
+          <img src={user} alt='' />
+          <input
+            id='Name'
+            type='text'
+            name='Name'
+            className='form-field'
+            placeholder='Enter Your Name'
+            required
+          />
+          <ValidationError prefix='Name' field='Name' errors={state.errors} />
+        </div>
+        <div className='form-element'>
+          <img src={buisness_suit} alt='' />
+          <input
+            id='company-name'
+            type='text'
+            name='company-name'
+            className='form-field'
+            placeholder='Enter Company Name'
+            required
+          />
+          <ValidationError
+            prefix='company-name'
+            field='company-name'
+            errors={state.errors}
+          />
+        </div>
+        <div className='form-element'>
+          <img src={email} alt='' />
+          <input
+            id='email'
+            type='email'
+            name='email'
+            className='form-field'
+            placeholder='Enter Your Email'
+            required
+          />
+          <ValidationError prefix='email' field='email' errors={state.errors} />
+        </div>
+        <div className='form-element'>
+          <img src={website_url} alt='' />
+          <input
+            id='website-url'
+            type='url'
+            name='website-url'
+            className='form-field'
+            placeholder='Website URL'
+            required
+          />
+          <ValidationError
+            prefix='website-url'
+            field='website-url'
+            errors={state.errors}
+          />
+        </div>
+        <div className='form-element'>
+          <img src={phone} alt='' />
+          <input
+            id='phone-number'
+            type='tel'
+            name='phone-number'
+            className='form-field'
+            placeholder='Enter Phone Number'
+            required
+          />
+          <ValidationError
+            prefix='phone-number'
+            field='phone-number'
+            errors={state.errors}
+          />
+        </div>
+        <div className='form-element'>
+          <img src={phone} alt='' />
+          <input
+            id='budget'
+            type='number'
+            name='budget'
+            className='form-field'
+            placeholder='Enter Monthly Budget'
+            required
+          />
+          <ValidationError
+            prefix='budget'
+            field='budget'
+            errors={state.errors}
+          />
+        </div>
+      </div>
+      <button type='submit' className='form-button' disabled={state.submitting}>
+        Save & Continue
+        <img src={right_arrow} alt='' />
+      </button>
+    </form>
+  );
 }
-export function Primeform() {
-	return (
-		<div className="form-container">
-			<div className="form-nav">
-				<div className="form-logo">
-					<img src={logo} alt="" />
-					<span className="form-logo-name">
-						The<strong>WEB3</strong>Guys
-					</span>
-				</div>
-				<div className="cross">
-					<img src={cross} alt="" />
-				</div>
-			</div>
-			<div className="progress-before"></div>
-			<div className="progress"></div>
-			<h1 className="consultion_call_header">
-				Book a free Consultation Call
-			</h1>
-			<h2 className="consultation_call_secondary">
-				Schedule a consultation call with a marketing specialist.
-			</h2>
-			<ContactForm />
-		</div>
-	);
+export function Primeform({ setDisplayForm }) {
+  return (
+    <div className='form-container'>
+      <div className='form-nav'>
+        <div className='form-logo'>
+          <img src={logo} alt='' />
+          <span className='form-logo-name'>
+            The<strong>WEB3</strong>Guys
+          </span>
+        </div>
+        <div className='cross'>
+          <img src={cross} alt='' onClick={() => setDisplayForm(false)} />
+        </div>
+      </div>
+      <div className='progress-before'></div>
+      <div className='progress'></div>
+      <h1 className='consultion_call_header'>Book a free Consultation Call</h1>
+      <h2 className='consultation_call_secondary'>
+        Schedule a consultation call with a marketing specialist.
+      </h2>
+      <ContactForm />
+    </div>
+  );
 }
