a420f1a418ad9aad7bf5380f099083037faa12e0
aryan <aryankalra02@gmail.com>
2023-04-15 04:20:35 +0530

    Why Choose Us

--- src/components/homepage2/homepage2.jsx
+++ src/components/homepage2/homepage2.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import CustomerReviewHome from './customer_review_home'
+import WhyChooseUs from './why_choose_us'
 function Homepage2() {
   return (
     <div>
+      <WhyChooseUs/>
         <CustomerReviewHome/>
     </div>
   )
---
+++ src/components/homepage2/why_choose_us.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import './why_choose_us.css'
+import icon_holder1 from "../../assets/icon-holder.svg";
+import icon_holder2 from "../../assets/icon-holder1.svg";
+import icon_holder3 from "../../assets/icon-holder2.svg";
+
+function WhyChooseUs() {
+	return (
+		<div>
+			<div className="why-choose-us">
+				<div className="head-subhead" id="head">
+					<div className="why-choose-us1">Why Choose Us ?</div>
+					<div className="unleash-your-potential">
+						Unleash Your Potential with Our Expert Support
+					</div>
+				</div>
+				<div className="key-factors-parent">
+					<div className="key-factors">
+						<b className="past-experience">Tailored Solutions</b>
+						<div className="our-team-has">
+							We work closely with each client to understand their
+							goals, audience, and budget, and develop a
+							customized marketing plan that delivers results.
+						</div>
+						<img
+							className="icon-holder"
+							alt=""
+							src={icon_holder1}
+						/>
+					</div>
+					<div className="key-factors">
+						<b className="past-experience">Community & Engagement</b>
+						<div className="our-team-has">
+							In the web3 world, community is king. We don't just
+							understand this, we're experts at building, growing,
+							and engaging communities around your brand.
+						</div>
+						<img
+							className="icon-holder"
+							alt=""
+							src={icon_holder2}
+						/>
+					</div>
+					<div className="key-factors">
+						<b className="past-experience">Innovation & Creativity</b>
+						<div className="our-team-has">
+							We keep our clients on top of the latest web3 market
+							trends, We'll help you stand out by adopting the
+							newest technology and social media strategies.
+						</div>
+						<img
+							className="icon-holder"
+							alt=""
+							src={icon_holder3}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default WhyChooseUs;
